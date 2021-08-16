4b5af965facdc95472e3f09506fc9db9e6ec5f68
Novazembla <nova@pro-pro.local>
2021-08-16 15:47:36 +0200

    more changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
-const { i18n } = require('./next-i18next.config');
 
 module.exports = {
-  i18n,
-
-  react: { useSuspense: false, wait:true }
+  i18n: {
+    localeDetection: true,
+    defaultLocale: "en",
+    locales: ["de", "en"],
+  },
 };
