11a35d7e258c16f95525d8d901ea0ef135311893
Novazembla <nova@pro-pro.local>
2021-10-25 15:22:24 +0200

    streamline quicksearch & menu close on route changes

--- components/app/LoadingBar.tsx
+++ components/app/LoadingBar.tsx
@@ -1,37 +1,26 @@
 import React, { useEffect, useState } from "react";
 import Router from "next/router";
 import { Box } from "@chakra-ui/react";
-import {
-  useMenuButtonContext,
-  useQuickSearchContext,
-} from "~/provider";
+import { useMenuButtonContext, useQuickSearchContext } from "~/provider";
 
 export const LoadingBar = ({ color }: { color: string }) => {
   const [barVisible, setBarVisible] = useState(false);
-  const { onMenuClose, isMenuOpen } = useMenuButtonContext();
-  const { isQuickSearchOpen, onQuickSearchClose } = useQuickSearchContext();
-  
-  const showBar = () => {
-    console.log(1);
-    if (isMenuOpen) {
-      console.log(2);
-      onMenuClose();
-    }
-    if (isQuickSearchOpen) {
-      console.log(3);
-      onQuickSearchClose()
-    };
+  const { onMenuClose } = useMenuButtonContext();
+  const { onQuickSearchClose } = useQuickSearchContext();
 
+  const showBar = () => {
+    onMenuClose();
+    onQuickSearchClose();
     setBarVisible(true);
-  }
-  
+  };
+
   const hideBar = () => setBarVisible(false);
 
   useEffect(() => {
     Router.events.on("routeChangeStart", showBar);
     Router.events.on("routeChangeComplete", hideBar);
     Router.events.on("routeChangeError", hideBar);
-    
+
     return () => {
       Router.events.off("routeChangeStart", showBar);
       Router.events.off("routeChangeComplete", hideBar);
